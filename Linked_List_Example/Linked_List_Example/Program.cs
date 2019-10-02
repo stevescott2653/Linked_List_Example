4bca9a8d5fccfc34fc48844f92e1878a9ae2b5f2
Steven Scott <stevescott2653@gmail.com>
2019-10-02 19:04:00 -0400

    Add files via upload

---
+++ Linked_List_Example/Linked_List_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_Example
+{
+    public class LinkedListExample
+    {
+        public static void Main(string[] args)
+        {
+            // Create a list of strings
+            var names = new LinkedList<string>();
+            names.AddLast("Chris");
+            names.AddLast("Linda");
+            names.AddLast("Sharron");
+            names.AddLast("Amber");
+            names.AddFirst("Frank"); // added to first index
+
+            // Iterate list elements using foreach loop
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
